65cc2326462dfd5cf28847e1518dbaba82a8026f
SungGil-5125 <sunggil0125@naver.com>
2022-05-16 18:24:02 +0900

    ✨ :: UserRepository

---
+++ src/main/java/com/project/daily/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.daily.repository;
+
+import com.project.daily.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
