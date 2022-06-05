5c5594aa8cc54c8458ed384a494ef45f7f128d77
SungGil-5125 <sunggil0125@naver.com>
2022-06-05 22:21:01 +0900

    ✨ :: loadUserByUsername 추가

---
+++ src/main/java/com/project/daily/service/CustomUserService.java
@@ -0,0 +1,25 @@
+package com.project.daily.service;
+
+import com.project.daily.exeception.CustomException;
+import com.project.daily.exeception.ErrorCode;
+import com.project.daily.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static com.project.daily.exeception.ErrorCode.USER_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+    }
+}
