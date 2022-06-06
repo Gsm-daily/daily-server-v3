b5add69c9b85829c203d73ebdf34861fc9629c07
SungGil-5125 <sunggil0125@naver.com>
2022-06-06 21:35:19 +0900

    ✨ :: jwt Request Filter

--- src/main/java/com/project/daily/config/jwt/JwtRequestFilter.java
+++ src/main/java/com/project/daily/config/jwt/JwtRequestFilter.java
@@ -1,4 +1,28 @@
 package com.project.daily.config.jwt;
 
-public class JwtRequestFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final UserDetailsService userDetailsService;
+    private final ObjectMapper objectMapper;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        
+    }
 }
