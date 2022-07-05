7ffb0d51d942f59b1b71ab4f81a7741a65fa8a1b
SungGil-5125 <sunggil0125@naver.com>
2022-07-05 19:56:48 +0900

    ✨ :: Token 재발급

--- src/main/java/com/project/daily/domain/user/controller/RefreshTokenController.java
+++ src/main/java/com/project/daily/domain/user/controller/RefreshTokenController.java
@@ -1,11 +1,25 @@
 package com.project.daily.domain.user.controller;
 
+import com.project.daily.domain.user.dto.Request.RefreshTokenDto;
+import com.project.daily.domain.user.service.RefreshTokenService;
+import com.project.daily.global.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
 public class RefreshTokenController {
 
-//    @PutMapping("/refresh")
-//    public
+    private final RefreshTokenService refreshTokenService;
+    private final TokenProvider tokenProvider;
+
+    @PutMapping("/refreshToken")
+    public Map<String, String> refreshToken(HttpServletRequest request, @RequestBody RefreshTokenDto refreshTokenDto) {
+        return refreshTokenService.refreshToken(tokenProvider.getRefreshToken(request), refreshTokenDto);
+    }
 }
--- src/main/java/com/project/daily/domain/user/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/project/daily/domain/user/service/impl/RefreshTokenServiceImpl.java
@@ -2,34 +2,48 @@ package com.project.daily.domain.user.service.impl;
 
 import com.project.daily.domain.user.User;
 import com.project.daily.domain.user.dto.Request.RefreshTokenDto;
-import com.project.daily.domain.user.exeception.CustomException;
+import com.project.daily.global.exeception.CustomException;
 import com.project.daily.domain.user.repository.UserRepository;
 import com.project.daily.domain.user.service.RefreshTokenService;
+import com.project.daily.global.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.project.daily.domain.user.exeception.ErrorCode.USER_NOT_FOUND;
+import static com.project.daily.global.exeception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class RefreshTokenServiceImpl implements RefreshTokenService {
 
     private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
 
     @Override
     public Map<String, String> refreshToken(String refreshToken, RefreshTokenDto refreshTokenDto) {
 
         String email = refreshTokenDto.getEmail();
-
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
 
-        if(userRepository)
-
+        if(user.getRefreshToken() == null) {
+            throw new CustomException(TOKEN_INVALID);
+        }
         Map<String, String> map = new HashMap<>();
 
+        if(user.getRefreshToken().equals(refreshToken) && tokenProvider.isExpired(refreshToken)) {
+            String newAccessToken = tokenProvider.generateAccessToken(email);
+            String newRefreshToken = tokenProvider.generateRefreshToken(email);
+
+            user.updateRefreshToken(newRefreshToken);
+            map.put("email : ", email);
+            map.put("NewAccessToken : ", newAccessToken);
+            map.put("NewRefreshToken : ", newRefreshToken);
+
+            return map;
+        }
+        throw new CustomException(REFRESH_TOKEN_NOT_FOUND);
     }
 }
