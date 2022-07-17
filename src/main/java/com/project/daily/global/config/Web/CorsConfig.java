6d009a3a084b3895ee82570402e3a97cebd21e7e
SungGil-5125 <sunggil0125@naver.com>
2022-07-17 22:07:04 +0900

    ✨ :: package 이름 변경

--- src/main/java/com/project/daily/global/config/Web/CorsConfig.java
+++ src/main/java/com/project/daily/global/config/Web/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.project.daily.global.config.Web;
+package com.project.daily.global.config.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
