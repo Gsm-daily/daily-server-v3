b4f3e03e5390572b8a705264d81200aa13519066
SungGil-5125 <sunggil0125@naver.com>
2022-07-19 14:05:04 +0900

    ✨ :: null -> roles update

--- src/main/java/com/project/daily/domain/sign/enumType/Role.java
+++ src/main/java/com/project/daily/domain/sign/enumType/Role.java
@@ -1,8 +1,14 @@
 package com.project.daily.domain.sign.enumType;
 
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
 
 @Getter
-public enum Role {
+public enum Role implements GrantedAuthority {
     ROLE_ADMIN, ROLE_USER;
+
+    @Override
+    public String getAuthority() {
+        return name(); // 인증 받은 사용자의 authorities를 조회할 수 있다.
+    }
 }
