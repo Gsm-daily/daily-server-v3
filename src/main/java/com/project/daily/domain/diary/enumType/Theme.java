9e7a5db58821adf1d765f4dcc327324fca18412a
SungGil-5125 <sunggil0125@naver.com>
2022-07-22 19:34:07 +0900

    ✨ :: Add Theme

---
+++ src/main/java/com/project/daily/domain/diary/enumType/Theme.java
@@ -0,0 +1,10 @@
+package com.project.daily.domain.diary.enumType;
+
+import lombok.Getter;
+
+@Getter
+public enum Theme {
+
+    GRASSLAND, OCEAN, HOME, FAIRYTALE
+    // 테마는 초원, 바다, 집, 동화 중 처음에 하나를 선택 하는 걸로 한다.
+}
